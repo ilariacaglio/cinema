73d399dd6918ff897d7678c9d69b4905299aa12f
ilariacaglio <ilaria.caglio@issgreppi.it>
2023-04-08 17:50:34 +0200

    NuGet

--- Cinema/Program.cs
+++ Cinema/Program.cs
@@ -1,7 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var serverVersion = ServerVersion.AutoDetect(connectionString);
 
 var app = builder.Build();
 
