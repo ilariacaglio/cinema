bf027650994f246a0357fe453d8ba09f6773fbbd
ilariacaglio <ilaria.caglio@issgreppi.it>
2023-04-16 16:23:40 +0200

    ComprendeRepository

---
+++ Cinema.DataAccess/Repository/ComprendeRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Cinema.DataAccess.Repository.IRepository;
+using Cinema.Models;
+
+namespace Cinema.DataAccess.Repository
+{
+	public class ComprendeRepository: IRepository<Comprende>, IComprendeRepository
+    {
+        private readonly AppDbContext _db;
+
+        public ComprendeRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Add(Comprende entity)
+        {
+            _db.Comprende.Add(entity);
+        }
+
+        public IEnumerable<Comprende> GetAll()
+        {
+            return _db.Comprende.ToList();
+        }
+
+        public Comprende? GetFirstOrDefault(int? idPosto, int? idPrenotazione)
+        {
+            if (idPosto is null || idPrenotazione is null)
+                return null;
+            return _db.Comprende.Single(c => c.IdPosto == idPosto && c.IdPrenotazione == idPrenotazione);
+        }
+
+        public Comprende? GetFirstOrDefault(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Comprende entity)
+        {
+            var comprendeFromDb = GetFirstOrDefault(entity.IdPosto, entity.IdPrenotazione);
+            if (comprendeFromDb != null)
+                _db.Comprende.Remove(comprendeFromDb);
+        }
+
+        public void RemoveRange(IEnumerable<Comprende> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Cinema.DataAccess/Repository/IRepository/IComprendeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Cinema.Models;
+
+namespace Cinema.DataAccess.Repository.IRepository
+{
+	public interface IComprendeRepository:IRepository<Comprende>
+    {
+        public Comprende? GetFirstOrDefault(int? idPosto, int? idPrenotazione);
+    }
+}
+
