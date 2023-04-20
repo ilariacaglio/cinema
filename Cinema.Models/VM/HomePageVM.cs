ca9ab4c4acc74a9ddbd0d150134e51833a4e9681
ilariacaglio <ilaria.caglio@issgreppi.it>
2023-04-20 11:29:31 +0200

    Home Page

---
+++ Cinema.Models/VM/HomePageVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Cinema.Models.VM
+{
+	public class HomePageVM
+	{
+		public int idFilm { get; set; }
+		public string NomeFilm { get; set; }
+		public DateOnly Data { get; set; }
+		public string Img { get; set; }
+		public bool uscito { get; set; }
+		public string Descrizione { get; set; }
+	}
+}
+
--- Cinema/Areas/User/Controllers/HomeController.cs
+++ Cinema/Areas/User/Controllers/HomeController.cs
@@ -1,21 +1,71 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Cinema.Models;
+using Cinema.DataAccess.Repository.IRepository;
+using Cinema.Models.VM;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Cinema.Controllers;
 [Area("User")]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unit)
     {
         _logger = logger;
+        unitOfWork = unit;
     }
 
+    [AllowAnonymous]
     public IActionResult Index()
     {
-        return View();
+        var film = unitOfWork.Film.GetAll().ToList();
+        if (film != null) {
+            var spettacoli = unitOfWork.Spettacolo.GetAll().ToList();
+            List<HomePageVM> lista = new List<HomePageVM>();
+            string titolo = null;
+            DateOnly data = new DateOnly();
+            foreach (var item in film)
+            {
+                titolo = item.Titolo;
+                var s = spettacoli.Find(s => s.IdFilm == item.Id);
+                if (s != null)
+                {
+                    if (  DateTime.Today.AddDays(-3) <= DateTime.Parse(s.Data.ToString()).Date && DateTime.Today.AddDays(3) >= DateTime.Parse(s.Data.ToString()).Date)
+                    {
+                        data = s.Data;
+                        lista.Add(new HomePageVM
+                        {
+                            Descrizione = item.Descrizione,
+                            idFilm = item.Id,
+                            NomeFilm = titolo,
+                            Data = data,
+                            Img = item.Img,
+                            uscito = true
+                        });
+                    }
+                    else if(DateTime.Today.AddDays(3) <= DateTime.Parse(s.Data.ToString()).Date)
+                    {
+                        data = s.Data;
+                        lista.Add(new HomePageVM
+                        {
+                            Descrizione = item.Descrizione,
+                            idFilm = item.Id,
+                            NomeFilm = titolo,
+                            Data = data,
+                            Img = item.Img,
+                            uscito = false
+                        });
+                    }
+                }
+            }
+            return View(lista);
+        }
+
+        return NotFound();
     }
 
     public IActionResult Privacy()
