7b1963ef2cc60ce7093841670db92c0275812a28
ilariacaglio <ilaria.caglio@issgreppi.it>
2023-05-01 16:50:43 +0200

    Set roles

--- Cinema.DataAccess/Repository/UtenteRepository.cs
+++ Cinema.DataAccess/Repository/UtenteRepository.cs
@@ -2,6 +2,7 @@
 using System.Linq.Expressions;
 using Cinema.DataAccess.Repository.IRepository;
 using Cinema.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Cinema.DataAccess.Repository
 {
@@ -19,9 +20,50 @@ namespace Cinema.DataAccess.Repository
             throw new NotImplementedException();
         }
 
-        public IEnumerable<Utente> GetAll()
+        public IEnumerable<IdentityUser> GetAllUsers()
         {
-            throw new NotImplementedException();
+            var utenti = _db.Users.ToList();
+            return utenti;
+        }
+
+        public IdentityRole? GetUserRole(string userId)
+        {
+            try
+            {
+                var IdRuolo = _db.UserRoles.Single(a => a.UserId == userId).RoleId;
+                return _db.Roles.Single(r => r.Id == IdRuolo);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<IdentityRole> GetRoles()
+        {
+            return _db.Roles.ToList();
+        }
+
+        public void UpdateRuolo (string utenteId, string ruoloId)
+        {
+            var userRole = _db.UserRoles.Single(u => u.UserId == utenteId);
+
+            //eliminazione ruolo
+            if (userRole != null)
+            {
+                _db.UserRoles.Remove(userRole);
+                _db.SaveChanges();
+            }
+
+            //aggiunta nuovo ruolo
+            var u = new IdentityUserRole<string>()
+            {
+                RoleId = ruoloId,
+                UserId = utenteId
+            };
+            _db.UserRoles.Add(u);
+            _db.SaveChanges();
+            
         }
 
         public IEnumerable<Utente> GetAll(Expression<Func<Utente, bool>>? filter = null, string? includeProperties = null)
@@ -34,11 +76,11 @@ namespace Cinema.DataAccess.Repository
             throw new NotImplementedException();
         }
 
-        public Utente? GetFirstOrDefault(string? id)
+        public IdentityUser? GetFirstOrDefaultUser(string? id)
         {
             if (id is null || id.Equals(String.Empty))
                 return null;
-            return _db.Utenti.Single(u => u.Id == id);
+            return _db.Users.Single(u => u.Id == id);
         }
 
         public void Remove(Utente entity)
@@ -47,6 +89,16 @@ namespace Cinema.DataAccess.Repository
         }
 
         public void RemoveRange(IEnumerable<Utente> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Utente> IRepository<Utente>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Utente? GetFirstOrDefault(string? id)
         {
             throw new NotImplementedException();
         }
---
+++ Cinema.Models/VM/UtenteVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Cinema.Models.VM
+{
+	public class UtenteVM
+	{
+		public IdentityUser? user { get; set; }
+		public string? ruolo { get; set; }
+		public IEnumerable<SelectListItem>? ruoli { get; set; }
+	}
+}
