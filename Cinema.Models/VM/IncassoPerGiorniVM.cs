5b0411de396d20e389f7de84f598877ea91e86ab
ilariacaglio <ilaria.caglio@issgreppi.it>
2023-05-02 13:54:01 +0200

    Incasso Per Giorni

---
+++ Cinema.Models/VM/IncassoPerGiorniVM.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Cinema.Models.VM
+{
+	public class IncassoPerGiorniVM
+	{
+		public DateOnly giorno { get; set; }
+		public double totaleGiorno { get; set; }
+	}
+}
--- Cinema/Areas/Admin/Controllers/StatisticheController.cs
+++ Cinema/Areas/Admin/Controllers/StatisticheController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography.Xml;
 using Cinema.DataAccess.Repository.IRepository;
 using Cinema.Models;
 using Cinema.Models.VM;
 using Cinema.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Cinema.Areas.Admin.Controllers
 {
@@ -12,6 +16,7 @@ namespace Cinema.Areas.Admin.Controllers
     [Authorize(Roles = SD.Role_Admin)]
     public class StatisticheController : Controller
     {
+        static HttpClient client = new HttpClient();
         private readonly IUnitOfWork _unitOfWork;
 
         public StatisticheController(IUnitOfWork unitOfWork)
@@ -24,15 +29,46 @@ namespace Cinema.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult IncassoPerGiorni()
+        {
+            return View();
+        }
+
         [HttpGet]
         public IActionResult JsonIncassiGiornalieri(DateOnly data)
         {
+            List<IncassiGiornalieriVM> lista = CalcoloGuadagni(data);
+            return Json(new { data = lista });
+        }
+
+        [HttpGet]
+        public IActionResult JsonIncassoPerGiorni(DateOnly inizio, DateOnly fine) {
+            List<IncassoPerGiorniVM> listaGiorni = new List<IncassoPerGiorniVM>();
+            for (DateOnly data = inizio; data <= fine; data = data.AddDays(1))
+            {
+                List<IncassiGiornalieriVM> lista = CalcoloGuadagni(data);
+               
+                //calcolo guadagno complessivo
+                IncassoPerGiorniVM _object = new IncassoPerGiorniVM() {
+                    giorno = data
+                };
+
+                foreach (var item in lista)
+                    _object.totaleGiorno += item.incasso;
+
+                listaGiorni.Add(_object);
+            }
+            return Json(new { data = listaGiorni });
+        }
+
+        private List<IncassiGiornalieriVM> CalcoloGuadagni(DateOnly data) {
             var spettacoli = _unitOfWork.Spettacolo.GetAll().Where(s => s.Data == data).ToList();
             List<Prenotazione> listaP = new List<Prenotazione>();
             foreach (var item in spettacoli)
             {
                 var prenotazioni = _unitOfWork.Prenotazione.GetAll().Where(p => p.DataS == item.Data && p.OraS == item.Ora && p.IdSala == item.IdSala).ToList();
-                foreach (var obj in prenotazioni) {
+                foreach (var obj in prenotazioni)
+                {
                     obj.Spettacolo = item;
                     obj.Comprendes = _unitOfWork.Comprende.GetAll().Where(c => c.IdPrenotazione == obj.Id).ToList();
                     foreach (var o in obj.Comprendes)
@@ -40,8 +76,7 @@ namespace Cinema.Areas.Admin.Controllers
                     listaP.Add(obj);
                 }
             }
-
-            var a = listaP.GroupBy(i => i.Spettacolo.IdFilm, (key, g) => new {FilmId = key, Income  = g.Sum(j => j.Comprendes.Sum(c=>c.Id.Costo))});
+            var a = listaP.GroupBy(i => i.Spettacolo.IdFilm, (key, g) => new { FilmId = key, Income = g.Sum(j => j.Comprendes.Sum(c => c.Id.Costo)) });
 
             List<IncassiGiornalieriVM> lista = new List<IncassiGiornalieriVM>();
             foreach (var item in a)
@@ -54,8 +89,7 @@ namespace Cinema.Areas.Admin.Controllers
                     incasso = item.Income
                 });
             }
-
-            return Json(new { data = lista });
+            return lista;
         }
     }
 }
